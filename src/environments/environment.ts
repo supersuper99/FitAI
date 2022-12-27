03a99db1fed2916eb6ff384409531a7b3ec4945c
PAPI <Eandrews100@gmail.com>
2022-12-27 12:05:37 -0800

    currently displaying the routine. need to add a better format for the routine.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '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'
 };
 
 /*
